8dddfd810e22547a78d630645da62b6b05db949e
Jose <jalopez1423@gmail.com>
2018-07-15 19:28:52 -0700

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="test">
+      <input type="text" v-model="title"><br/>
+      <h1>{{title}}</h1>
+      <p v-if="showName">{{user.firstName}}</p>
+      <p v-else>Nobody</p>
+
+      <ul>
+        <li v-for="item in items">{{item.title}}</li>
+      </ul>
+
+      <button v-on:click="greet">Say Greeting</button>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "test",
+      data() {
+          return {
+            title: 'Hello World',
+            user: {
+              firstName: 'John',
+              lastName: 'Doe'
+            },
+            showName: true,
+            items: [
+              {title: 'Item One'},
+              {title: 'Item Two'},
+              {title: 'Item Three'}
+            ]
+          }
+      },
+      methods: {
+        greet: function () {
+          alert('hello');
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
